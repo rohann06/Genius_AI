360b18a90b24c13b964af3bb668b9f555d9ae9e7
Rohan <rohanahire651@gmail.com>
2023-09-07 15:41:34 +0530

    App UI structure created

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { SiOpsgenie } from "react-icons/si";
+
+const Navbar = () => {
+  return (
+    <div className=" mx-10 my-8 felx flex-col justify-center items-center">
+      <div className=" font-LexendDeca text-[#F5F5F5] font-semibold text-[30px] flex items-center gap-3">
+        <span className=" text-red-600 text-[35px]">
+          <SiOpsgenie />
+        </span>
+        Genius
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
