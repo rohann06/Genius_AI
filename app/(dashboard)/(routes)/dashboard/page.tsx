1e231b3511e033b153d22732d3223e0dd709eec5
Rohan <rohanahire651@gmail.com>
2023-09-10 21:45:05 +0530

    used a next  route group convection

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/app/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>Dashboard page (protacted)</p>
+    </div>
+  );
+}
---
+++ app/(landibg)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage (unprotected)</div>
+  )
+}
+
+export default LandingPage
